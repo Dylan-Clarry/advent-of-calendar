81d9138aa6cc06179d3e17b8c327744d82f18c74
Dylan <dylan.imail@gmail.com>
2022-12-07 10:06:47 -0500

    first commit

---
+++ day01/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "io"
+    "os"
+)
+
+func check(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
+
+func main() {
+    fmt.Printf("Advent of Calendar day 0")
+}
