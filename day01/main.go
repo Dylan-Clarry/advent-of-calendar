7eb54d8b4f1741d103160163bd004e0f291af4ea
Dylan <dylan.imail@gmail.com>
2022-12-08 07:52:50 -0500

    Part one

--- day01/main.go
+++ day01/main.go
@@ -1,18 +1,42 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "io"
-    "os"
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+    "math"
 )
-
-func check(err error) {
-    if err != nil {
-        panic(err)
-    }
-}
 
 func main() {
     fmt.Printf("Advent of Calendar day 0")
+    file, err := os.Open("bulking-season.txt")
+    if err != nil {
+        panic(err)
+    }
+    defer file.Close()
+
+    scanner := bufio.NewScanner(file)
+
+    max := 0
+    curr := 0
+    for scanner.Scan() {
+        line := scanner.Text()
+        if(line == "") {
+            max = int(math.Max(float64(max), float64(curr)))
+            curr = 0
+        } else {
+            num, err := strconv.Atoi(line)
+            if(err != nil) {
+                panic(err)
+            }
+            curr += num
+        }
+    }
+
+    fmt.Printf("Most calories: %d", max)
+    
+    if err := scanner.Err(); err != nil {
+        panic(err)
+    }
 }
